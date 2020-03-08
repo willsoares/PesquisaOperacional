d5ce78534f9fb297618beb180cfaffa7b6b8cd1d
willsoares <willsoares96@hotmail.com>
2020-03-08 16:22:14 -0300

    Teste 1

--- conteiner/src/main/java/conteiner/App.java
+++ conteiner/src/main/java/conteiner/App.java
@@ -1,5 +1,9 @@
 package conteiner;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,79 +13,124 @@ import ilog.concert.IloNumVar;
 import ilog.cplex.IloCplex;
 
 public class App {
-
-    public static void main(String[] args){
+    public static Data ReadFile(String filename) {
         try {
+            var reader = new BufferedReader(new FileReader(filename));
+            var data = new Data();
+
+            var line = reader.readLine();
+            data.setQtdItens(Integer.parseInt(line.substring(2)));
+
+            line = reader.readLine();
+            var st = new StringTokenizer(line, "\t");
+            st.nextToken();
+            data.setQtdConteiners(Integer.parseInt(st.nextToken()));
+            data.setConteinerCapacidade(Integer.parseInt(st.nextToken()));
+            data.setConteinerVolume(Integer.parseInt(st.nextToken()));
+
+            line = reader.readLine();
+            st = new StringTokenizer(line, "\t");
+            st.nextToken();
+            data.setUsoMaximoItem(Integer.parseInt(st.nextToken()));
+
+            int i = 0;
+            line = reader.readLine();
+            var l = new double[data.getQtdItens()];
+            var p = new double[data.getQtdItens()];
+            var v = new double[data.getQtdItens()];
+            while (line != null) {
+                st = new StringTokenizer(line, "\t");
+                st.nextToken();
+                l[i] = Double.parseDouble(st.nextToken());
+                p[i] = Double.parseDouble(st.nextToken());
+                v[i] = Double.parseDouble(st.nextToken());
+                i++;
+                line = reader.readLine();
+            }
+            data.setItensLucro(l);
+            data.setItensPeso(p);
+            data.setItensVolume(v);
+
+            reader.close();
+            return data;
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            var data = ReadFile("instancias/instancia_00.conteiner");
+
             IloCplex model = new IloCplex();
-            
-            //declarando variaveis de decisao
-            IloNumVar[][] s = new IloNumVar[Data.n][Data.k];
-            //instanciando
-            for (int i = 0; i < Data.n; i++) {
-                for (int j = 0; j < Data.k; j++) {
-                    //s[i][j] = model.numVar(0, Data.b);
-                    s[i][j] = model.intVar(0, Data.b);
+
+            // declarando variaveis de decisao
+            IloNumVar[][] s = new IloNumVar[data.getQtdItens()][data.getQtdConteiners()];
+            // instanciando
+            for (int i = 0; i < data.getQtdItens(); i++) {
+                for (int j = 0; j < data.getQtdConteiners(); j++) {
+                    s[i][j] = model.intVar(0, data.getUsoMaximoItem());
                 }
             }
-            //expressao linear para funcaoobjetivo
+            // expressao linear para funcaoobjetivo
             IloLinearNumExpr fo = model.linearNumExpr();
-            for (int i = 0; i < Data.n; i++) {
-                for (int j = 0; j < Data.k; j++) {
-                    fo.addTerm(Data.l[i], s[i][j]);
+            for (int i = 0; i < data.getQtdItens(); i++) {
+                for (int j = 0; j < data.getQtdConteiners(); j++) {
+                    fo.addTerm(data.getItensLucro()[i], s[i][j]);
                 }
             }
             model.addMaximize(fo);
-            
-            //adicionando restricoes
-            //restricoes de carga
-            for (int j = 0; j < Data.k; j++) {
-                //para cada container, adicionar uma inequacao
+
+            // adicionando restricoes
+            // restricoes de carga
+            for (int j = 0; j < data.getQtdConteiners(); j++) {
+                // para cada container, adicionar uma inequacao
                 IloLinearNumExpr restricaoCarga = model.linearNumExpr();
-                for (int i = 0; i < Data.n; i++) {
-                    restricaoCarga.addTerm(Data.p[i], s[i][j]);
+                for (int i = 0; i < data.getQtdItens(); i++) {
+                    restricaoCarga.addTerm(data.getItensPeso()[i], s[i][j]);
                 }
-                //adicionando inequacao
-                model.addLe(restricaoCarga, Data.cc);
+                // adicionando inequacao
+                model.addLe(restricaoCarga, data.getConteinerCapacidade());
             }
-            
-            //restricoes de volume
-            for (int j = 0; j < Data.k; j++) {
-                //para cada container, adicionar uma inequacao
+
+            // restricoes de volume
+            for (int j = 0; j < data.getQtdConteiners(); j++) {
+                // para cada container, adicionar uma inequacao
                 IloLinearNumExpr restricaoVolume = model.linearNumExpr();
-                for (int i = 0; i < Data.n; i++) {
-                    restricaoVolume.addTerm(Data.v[i], s[i][j]);
+                for (int i = 0; i < data.getQtdItens(); i++) {
+                    restricaoVolume.addTerm(data.getItensVolume()[i], s[i][j]);
                 }
-                //adicionando inequacao
-                model.addLe(restricaoVolume, Data.cv);
+                // adicionando inequacao
+                model.addLe(restricaoVolume, data.getConteinerVolume());
             }
-            
-            //restricoes de qtd item
-            for (int i = 0; i < Data.n; i++) {
+
+            // restricoes de qtd item
+            for (int i = 0; i < data.getQtdItens(); i++) {
                 IloLinearNumExpr restricaoLimite = model.linearNumExpr();
-                for (int j = 0; j < Data.k; j++) {
+                for (int j = 0; j < data.getQtdConteiners(); j++) {
                     restricaoLimite.addTerm(1.0, s[i][j]);
                 }
-                //adicionando inequacao
-                model.addLe(restricaoLimite, Data.b);
+                // adicionando inequacao
+                model.addLe(restricaoLimite, data.getUsoMaximoItem());
             }
-            
-            //resolvendo o problema
-            if(model.solve()){
-                System.out.println("=================================");
+
+            // resolvendo o problema
+            if (model.solve()) {
+                System.out.println("\nResultado:\n");
                 System.out.println(model.getStatus());
                 System.out.println(model.getObjValue());
-                System.out.println("=================================");
-                for (int i = 0; i < Data.n; i++) {
-                    for (int j = 0; j < Data.k; j++) {
+                for (int i = 0; i < data.getQtdItens(); i++) {
+                    System.out.print("|");
+                    for (int j = 0; j < data.getQtdConteiners(); j++) {
                         System.out.print(model.getValue(s[i][j]) + "\t");
                     }
-                    System.out.println("");
+                    System.out.println("|");
                 }
+            } else {
+                System.out.println("???????????????");
             }
-            else {
-                System.out.println("Deu merda");
-            }
-            
+
         } catch (IloException ex) {
             Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
         }
--- conteiner/src/main/java/conteiner/Data.java
+++ conteiner/src/main/java/conteiner/Data.java
@@ -1,55 +1,125 @@
 package conteiner;
 
 public class Data {
+	private int qtdItens;
+	private int qtdConteiners;
+	private int usoMaximoItem;
+	private int conteinerCapacidade;
+	private int conteinerVolume;
+	
+	private double itensLucro[];
+	private double itensPeso[];
+	private double itensVolume[];
 
-   
-    public static int n  = 10;
-    public static int k  = 5;
-   
-    public static int b  = 3;
-   
-    public static int cc = 500;
-    public static int cv = 550;
-   
-   
-   
-    public static double l[] = {
-        6.236,
-        3.493,
-        7.278,
-        2.198,
-        3.678,
-        6.656,
-        9.796,
-        0.094,
-        9.281,
-        5.961
-    };
-   
-    public static double p[] = {
-        207.642,
-        095.400,
-        007.155,
-        165.310,
-        018.864,
-        149.896,
-        185.586,
-        218.137,
-        118.679,
-        220.735
-    };
-   
-    public static double v[] = {
-        169.477,
-        465.024,
-        172.978,
-        205.382,
-        092.878,
-        232.339,
-        084.960,
-        199.751,
-        035.852,
-        172.303
-    };
-   
+	/**
+	 * @return the qtdItens
+	 */
+	public int getQtdItens() {
+		return qtdItens;
+	}
+
+	/**
+	 * @return the itensVolume
+	 */
+	public double[] getItensVolume() {
+		return itensVolume;
+	}
+
+	/**
+	 * @param itensVolume the itensVolume to set
+	 */
+	public void setItensVolume(double itensVolume[]) {
+		this.itensVolume = itensVolume;
+	}
+
+	/**
+	 * @return the itensPeso
+	 */
+	public double[] getItensPeso() {
+		return itensPeso;
+	}
+
+	/**
+	 * @param itensPeso the itensPeso to set
+	 */
+	public void setItensPeso(double itensPeso[]) {
+		this.itensPeso = itensPeso;
+	}
+
+	/**
+	 * @return the itensLucro
+	 */
+	public double[] getItensLucro() {
+		return itensLucro;
+	}
+
+	/**
+	 * @param itensLucro the itensLucro to set
+	 */
+	public void setItensLucro(double itensLucro[]) {
+		this.itensLucro = itensLucro;
+	}
+
+	/**
+	 * @return the conteinerVolume
+	 */
+	public int getConteinerVolume() {
+		return conteinerVolume;
+	}
+
+	/**
+	 * @param conteinerVolume the conteinerVolume to set
+	 */
+	public void setConteinerVolume(int conteinerVolume) {
+		this.conteinerVolume = conteinerVolume;
+	}
+
+	/**
+	 * @return the conteinerCapacidade
+	 */
+	public int getConteinerCapacidade() {
+		return conteinerCapacidade;
+	}
+
+	/**
+	 * @param conteinerCapacidade the conteinerCapacidade to set
+	 */
+	public void setConteinerCapacidade(int conteinerCapacidade) {
+		this.conteinerCapacidade = conteinerCapacidade;
+	}
+
+	/**
+	 * @return the usoMaximoItem
+	 */
+	public int getUsoMaximoItem() {
+		return usoMaximoItem;
+	}
+
+	/**
+	 * @param usoMaximoItem the usoMaximoItem to set
+	 */
+	public void setUsoMaximoItem(int usoMaximoItem) {
+		this.usoMaximoItem = usoMaximoItem;
+	}
+
+	/**
+	 * @return the qtdConteiners
+	 */
+	public int getQtdConteiners() {
+		return qtdConteiners;
+	}
+
+	/**
+	 * @param qtdConteiners the qtdConteiners to set
+	 */
+	public void setQtdConteiners(int qtdConteiners) {
+		this.qtdConteiners = qtdConteiners;
+	}
+
+	/**
+	 * @param qtdItens the qtdItens to set
+	 */
+	public void setQtdItens(int qtdItens) {
+		this.qtdItens = qtdItens;
+	}
 }
