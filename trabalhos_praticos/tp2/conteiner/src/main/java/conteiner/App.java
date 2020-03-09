1ecc817146befe9699c8b9f7e5b33cec106ba6d8
willsoares <willsoares96@hotmail.com>
2020-03-09 08:36:38 -0300

    Finaliza tp2 e relatorio 2 Adiciona gitignore

--- trabalhos_praticos/tp2/conteiner/src/main/java/conteiner/App.java
+++ trabalhos_praticos/tp2/conteiner/src/main/java/conteiner/App.java
@@ -61,74 +61,82 @@ public class App {
 
     public static void main(String[] args) {
         try {
-            var data = ReadFile("instancias/instancia_00.conteiner");
+            var file = "instancia_02.conteiner";
+            var data = ReadFile("instancias/" + file);
 
-            IloCplex model = new IloCplex();
+            IloCplex modelo = new IloCplex();
 
-            // declarando variaveis de decisao
+            // somatorio
             IloNumVar[][] s = new IloNumVar[data.getQtdItens()][data.getQtdConteiners()];
-            // instanciando
             for (int i = 0; i < data.getQtdItens(); i++) {
                 for (int j = 0; j < data.getQtdConteiners(); j++) {
-                    s[i][j] = model.intVar(0, data.getUsoMaximoItem());
+                    // s[i][j] = modelo.numVar(0, data.getUsoMaximoItem());
+                    s[i][j] = modelo.intVar(0, data.getUsoMaximoItem());
                 }
             }
-            // expressao linear para funcaoobjetivo
-            IloLinearNumExpr fo = model.linearNumExpr();
+
+            // expressão linear
+            IloLinearNumExpr fo = modelo.linearNumExpr();
             for (int i = 0; i < data.getQtdItens(); i++) {
                 for (int j = 0; j < data.getQtdConteiners(); j++) {
                     fo.addTerm(data.getItensLucro()[i], s[i][j]);
                 }
             }
-            model.addMaximize(fo);
 
-            // adicionando restricoes
-            // restricoes de carga
-            for (int j = 0; j < data.getQtdConteiners(); j++) {
-                // para cada container, adicionar uma inequacao
-                IloLinearNumExpr restricaoCarga = model.linearNumExpr();
+            // maximizar
+            modelo.addMaximize(fo);
+            // limitando o programa para 5 minutos
+            modelo.setParam(IloCplex.Param.TimeLimit, 600);
+            // adcionar restrições
+            // restrição de carga
+
+            for (int j = 0; j < data.getQtdConteiners(); j++) {// j=1..k
+                // para cada container adicionar uma inequação
+                IloLinearNumExpr restricaoCarga = modelo.linearNumExpr();
                 for (int i = 0; i < data.getQtdItens(); i++) {
                     restricaoCarga.addTerm(data.getItensPeso()[i], s[i][j]);
                 }
-                // adicionando inequacao
-                model.addLe(restricaoCarga, data.getConteinerCapacidade());
+                // adicionar inequação
+                modelo.addLe(restricaoCarga, data.getConteinerCapacidade());
             }
 
-            // restricoes de volume
+            // restrição de volume
             for (int j = 0; j < data.getQtdConteiners(); j++) {
-                // para cada container, adicionar uma inequacao
-                IloLinearNumExpr restricaoVolume = model.linearNumExpr();
+                IloLinearNumExpr restricaoVolume = modelo.linearNumExpr();
                 for (int i = 0; i < data.getQtdItens(); i++) {
                     restricaoVolume.addTerm(data.getItensVolume()[i], s[i][j]);
                 }
-                // adicionando inequacao
-                model.addLe(restricaoVolume, data.getConteinerVolume());
+                modelo.addLe(restricaoVolume, data.getConteinerVolume());
             }
-
-            // restricoes de qtd item
+            // restrição de limite
             for (int i = 0; i < data.getQtdItens(); i++) {
-                IloLinearNumExpr restricaoLimite = model.linearNumExpr();
+                IloLinearNumExpr restricaoLimite = modelo.linearNumExpr();
                 for (int j = 0; j < data.getQtdConteiners(); j++) {
                     restricaoLimite.addTerm(1.0, s[i][j]);
                 }
-                // adicionando inequacao
-                model.addLe(restricaoLimite, data.getUsoMaximoItem());
+                modelo.addLe(restricaoLimite, data.getUsoMaximoItem());
             }
+            // restrição de limite de cada item
+            for (int i = 0; i < data.getQtdItens(); i++) {
+                for (int j = 0; j < data.getQtdConteiners(); j++) {
+                    modelo.addLe(s[i][j], data.getUsoMaximoItem());
+                    modelo.addGe(s[i][j], 0.0);
+                }
+            }
+            if (modelo.solve()) {
+                System.out.println("----------");
+                System.out.println(modelo.getStatus());
+                System.out.println(modelo.getObjValue());
+                System.out.println("----------");
 
-            // resolvendo o problema
-            if (model.solve()) {
-                System.out.println("\nResultado:\n");
-                System.out.println(model.getStatus());
-                System.out.println(model.getObjValue());
                 for (int i = 0; i < data.getQtdItens(); i++) {
-                    System.out.print("|");
                     for (int j = 0; j < data.getQtdConteiners(); j++) {
-                        System.out.print(model.getValue(s[i][j]) + "\t");
+                        System.out.print(modelo.getValue(s[i][j]) + "\t");
                     }
-                    System.out.println("|");
+                    System.out.println();
                 }
             } else {
-                System.out.println("???????????????");
+                System.out.println("Erro");
             }
 
         } catch (IloException ex) {
